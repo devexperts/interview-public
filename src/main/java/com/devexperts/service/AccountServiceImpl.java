5e43221b6fd31ceb6bc5a4f7a016a0f182a11fa1
Andrey Bodyul <andrey.bodyul@ocado.com>
2020-06-16 03:05:55 +0300

    task 2

--- src/main/java/com/devexperts/service/AccountServiceImpl.java
+++ src/main/java/com/devexperts/service/AccountServiceImpl.java
@@ -2,8 +2,10 @@ package com.devexperts.service;
 
 import com.devexperts.account.Account;
 import com.devexperts.account.AccountKey;
+
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -13,7 +15,7 @@ import java.util.concurrent.CopyOnWriteArrayList;
 @Service
 public class AccountServiceImpl implements AccountService {
 
-    private final Map<AccountKey,Account> accounts = new ConcurrentHashMap<>();
+    private final Map<AccountKey, Account> accounts = new ConcurrentHashMap<>();
 
     @Override
     public void clear() {
@@ -32,6 +34,25 @@ public class AccountServiceImpl implements AccountService {
 
     @Override
     public void transfer(Account source, Account target, double amount) {
-        //do nothing for now
+        BigDecimal bDAmount = BigDecimal.valueOf(amount);
+        double newSourceBalance =
+                BigDecimal.valueOf(source.getBalance()).subtract(bDAmount).doubleValue();
+        double newTargetBalance =
+                BigDecimal.valueOf(target.getBalance()).add(bDAmount).doubleValue();
+        if (isDoubleInfiniteOrNan(newSourceBalance) || isDoubleInfiniteOrNan(
+                newTargetBalance)) {
+            //TODO: some logging could be there
+            //TODO: better to propagate checked exception to interface method
+            throw new RuntimeException("Cannot transfer");
+        }
+        source.setBalance(newSourceBalance);
+        target.setBalance(newTargetBalance);
+
+        accounts.put(source.getAccountKey(), source);
+        accounts.put(target.getAccountKey(), target);
+    }
+
+    private boolean isDoubleInfiniteOrNan(Double value) {
+        return Double.isInfinite(value) || Double.isNaN(value);
     }
 }
