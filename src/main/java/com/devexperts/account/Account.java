9fd4840c06a50b6f54cad44986e5623cca885e21
Vladimir Solonchenko <vovkasol@mail.ru>
2020-06-27 12:19:45 +0300

    Task2 add accounts lock

--- src/main/java/com/devexperts/account/Account.java
+++ src/main/java/com/devexperts/account/Account.java
@@ -1,10 +1,14 @@
 package com.devexperts.account;
 
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class Account {
     private final AccountKey accountKey;
     private final String firstName;
     private final String lastName;
     private Double balance;
+    private final AtomicBoolean depositLock = new AtomicBoolean();
 
     public Account(AccountKey accountKey, String firstName, String lastName, Double balance) {
         this.accountKey = accountKey;
@@ -12,6 +16,20 @@ public class Account {
         this.lastName = lastName;
         this.balance = balance;
     }
+
+    /**
+     * lock balance for prevent future concurrent modification problem
+     *
+     * @return false - balance modification forbidden, true- lock successful
+     */
+    public boolean lockBalance() {
+        return depositLock.compareAndSet(false, true);
+    }
+
+    public boolean unLockBalance() {
+        return depositLock.compareAndSet(true, false);
+    }
+
 
     public AccountKey getAccountKey() {
         return accountKey;
