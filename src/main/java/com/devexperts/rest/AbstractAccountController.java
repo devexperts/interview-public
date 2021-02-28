b71aab03935d419d7682d391added8ca21af7e3d
GalinaIs <denis@isyanov.com>
2021-02-28 16:05:06 +0300

    Реализован перевод между аккаунтами через Rest-запрос.

--- src/main/java/com/devexperts/rest/AbstractAccountController.java
+++ src/main/java/com/devexperts/rest/AbstractAccountController.java
@@ -1,7 +1,8 @@
 package com.devexperts.rest;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 
 public abstract class AbstractAccountController {
-    abstract ResponseEntity<Void> transfer(long sourceId, long targetId, double amount);
+    abstract ResponseEntity<Void> transfer(@PathVariable Long sourceId, @PathVariable Long targetId, @PathVariable Double amount);
 }
