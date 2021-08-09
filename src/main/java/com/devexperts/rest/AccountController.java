20a30e451180f01aa68167051a5b28ec81ca2db7
Olga <olga122123@gmail.com>
2021-08-09 12:34:15 +0400

    task 4

--- src/main/java/com/devexperts/rest/AccountController.java
+++ src/main/java/com/devexperts/rest/AccountController.java
@@ -1,14 +1,45 @@
 package com.devexperts.rest;
 
+import com.devexperts.account.Account;
+import com.devexperts.service.AccountService;
+import com.devexperts.service.exceptions.AccountNotFoundException;
+import com.devexperts.service.exceptions.InsufficientBalanceException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.math.BigDecimal;
 
 @RestController
 @RequestMapping("/api")
 public class AccountController extends AbstractAccountController {
+    private final AccountService accountService;
 
-    public ResponseEntity<Void> transfer(long sourceId, long targetId, double amount) {
-        return null;
+    @Autowired
+    public AccountController(AccountService accountService) {
+        this.accountService = accountService;
+    }
+
+    @GetMapping("/operations/transfer")
+    public ResponseEntity<Void> transfer(
+            @RequestParam("sourceId") Long sourceId,
+            @RequestParam("targetId") Long targetId,
+            @RequestParam("amount") BigDecimal amount) {
+
+        if (sourceId == null || targetId == null || amount == null || amount.compareTo(BigDecimal.ZERO) < 0) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST); // wrong parameters
+        }
+        try {
+            Account source = accountService.getAccount(sourceId);
+            Account target = accountService.getAccount(targetId);
+            accountService.transfer(source,target,amount);
+            return new ResponseEntity<>(HttpStatus.OK); // successful transfer
+        } catch (AccountNotFoundException e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND); // account is not found
+        } catch (InsufficientBalanceException e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR); // insufficient account balance
+        }
     }
 }
