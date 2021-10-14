1df6ffe1bc7a7ee54605d89ed4686fabf4cc08bc
Seven Lightnapper <dairenray@gmail.com>
2021-10-14 21:55:18 +0300

    Task 3

--- src/main/java/com/devexperts/service/AccountServiceImpl.java
+++ src/main/java/com/devexperts/service/AccountServiceImpl.java
@@ -4,15 +4,15 @@ import com.devexperts.account.Account;
 import com.devexperts.account.AccountKey;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.locks.ReentrantLock;
 
 @Service
 public class AccountServiceImpl implements AccountService {
 
-    private final Map<AccountKey, Account> accounts = new HashMap<>();
+    private final Map<AccountKey, Account> accounts = new ConcurrentHashMap<>();
+    private final ReentrantLock lock = new ReentrantLock();
 
     @Override
     public void clear() {
@@ -37,15 +37,22 @@ public class AccountServiceImpl implements AccountService {
 
     @Override
     public void transfer(Account source, Account target, double amount) {
-        if (amount < 0 || source == target) {
+        if (amount < 0 || source == target)
             throw new IllegalArgumentException("Amount can't be negative OR Source can't be same as target.");
-        }
-        double sourceBalance = source.getBalance();
-        double targetBalance = target.getBalance();
-        if (sourceBalance < amount)
-            throw new IllegalArgumentException("Source balance is insufficient.");
 
-        source.setBalance(sourceBalance - amount);
-        target.setBalance(targetBalance + amount);
+        lock.lock();
+        try {
+            double sourceBalance = source.getBalance();
+            double targetBalance = target.getBalance();
+
+            if (sourceBalance < amount)
+                throw new IllegalArgumentException("Source balance is insufficient.");
+
+            source.setBalance(sourceBalance - amount);
+            target.setBalance(targetBalance + amount);
+
+        } finally {
+            lock.unlock();
+        }
     }
 }
