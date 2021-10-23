9197019e1cf9714e997e1d73f90f8229c6072bcb
45OchilAZ <abdusame93@gmail.com>
2021-10-23 12:55:43 +0300

    Task 1

--- src/main/java/com/devexperts/service/AccountServiceImpl.java
+++ src/main/java/com/devexperts/service/AccountServiceImpl.java
@@ -5,12 +5,15 @@ import com.devexperts.account.AccountKey;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class AccountServiceImpl implements AccountService {
 
-    private final List<Account> accounts = new ArrayList<>();
+    //private final List<Account> accounts = new ArrayList<>();
+    private Map<AccountKey, Account> accounts = new HashMap<>();
 
     @Override
     public void clear() {
@@ -19,15 +22,21 @@ public class AccountServiceImpl implements AccountService {
 
     @Override
     public void createAccount(Account account) {
-        accounts.add(account);
+        //accounts.add(account);
+        accounts.put(account.getAccountKey(), account);
     }
 
     @Override
     public Account getAccount(long id) {
-        return accounts.stream()
-                .filter(account -> account.getAccountKey() == AccountKey.valueOf(id))
+        // переопределил метод equals в классе AccountKey
+        // сложность метода O(N) где N размер списка аккаунтов
+        /*return accounts.stream()
+                .filter(account -> account.getAccountKey().equals(AccountKey.valueOf(id)))
                 .findAny()
-                .orElse(null);
+                .orElse(null);*/
+
+        // если использовать HashMap вместо ArrayList, то сложность этого метода будет O(1)
+        return accounts.get(AccountKey.valueOf(id));
     }
 
     @Override
